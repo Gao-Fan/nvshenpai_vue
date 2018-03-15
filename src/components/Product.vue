cca29e4ee5130b98ed63fae2e06d2fa2fc7da4a3
gaofan <1125356092@qq.com>
2018-03-15 15:12:00 +0800

    1.4

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,39 +1,56 @@
 <template>
 	<div>
-		<ProHeader></ProHeader>
-
+		<!-- <ProHeader :brand='brand'></ProHeader> -->
+		<!-- <Photo :photos='photos'></Photo>
+		<Part :part_message='part_message'></Part>
+		<ProBottom :part_message='part_message'></ProBottom> -->
+		<!-- 购物按钮 -->
 	</div>
 </template>
 <script>
 import axios from 'axios'
 
 import ProHeader from './product/proheader.vue'
+import Photo from './product/Photo.vue'
+import Part from './product/Part.vue'
+import ProBottom from './product/ProBottom.vue'
+
+
 export default {
   name: 'Product',
   components:{
-	ProHeader
+	ProHeader,
+	Photo,
+	Part,
+	ProBottom
   },
   data(){
     return{
-    	fid:''
+    	fid:'',
+    	brand:'',
+    	photos:'',
+    	part_message:''
     }
   },
   updated(){
 
   },
-  // beforeMount(){
-  // 	this.fid = this.$route.params.fid
-  // },
   mounted(){  	
   	this.fid = this.$route.params.fid
-  	console.log(this.fid)
+  	//console.log(this.fid)
   	axios.get("/product?platform=wap&channel=BDPZ_100051&access_token=&city=&id="+this.fid+"&only_specifications=&use_limit_days=&date=")
   		 .then( (res)=>{
-  		 	console.log(res)
+  		 	//console.log(res)
+  		 	this.brand = res.data.data.brand
+  		 	//console.log(res.data.data.image)
+  		 	this.photos = res.data.data.image
+  		 	//console.log(res.data.data)
+  		 	this.part_message = res.data.data
+  		 	//console.log(this.part_message)
   		 })
   }
 }
 </script>
 <style scoped>
-	
+
 </style>
---
+++ src/components/product/ProBottom.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class='bottom'>
+    <div class="left">
+    	❤
+    </div>
+    <div class="middle">
+    	加入衣袋
+    </div>
+    <div class="right">
+    	立即购买
+    </div>
+   </div>
+</template>
+
+<script>
+  import axios from 'axios'
+export default {
+  name: 'ProBottom',
+  props:['part_message']
+}
+</script>
+
+<style scoped>
+	.right{
+		background: #f5359b;
+		color: #fff;
+		float: left;
+		width: 3.466667rem;
+		height: 1.333333rem;
+		line-height: 1.333333rem;
+		text-align: center;
+		font-size: .36rem;
+	}
+	.middle{
+		border-top: 1px solid #d3d3d3;
+		border-left: 1px solid #d3d3d3;
+		border-right: 1px solid #d3d3d3;
+		float: left;
+		color: #f5359b;
+		width: 3.466667rem;
+		height: 1.333333rem;
+		line-height: 1.333333rem;
+		text-align: center;
+		font-size: .36rem;
+		background: #fff;
+	}
+	.left{
+		border-top: 1px solid #d3d3d3;
+		width: 3.0666667rem;
+		float: left;
+		height: 1.3333333rem;
+		display: flex;
+		flex-direction:row;
+		justify-content: center;
+		align-items: center;
+		font-size: 1rem;
+		color: #ccc;
+		background: #fff;
+	}
+	div{
+		margin: 0;
+		box-sizing: border-box;
+	}
+	.bottom{
+		position: fiexd;
+		bottom: 0;
+		left: 0;
+		height: 1.333333rem;
+		width: 100%;
+		z-index: 50;
+		position: sticky;
+		bottom: 0;
+	}
+</style>
